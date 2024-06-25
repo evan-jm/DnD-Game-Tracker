a18dfa055c0e8c149e241023ba04c6667d737494
Evan Mitchell <ejm0214@gmail.com>
2024-06-25 10:25:26 -0400

    Working on Board component

---
+++ frontend/src/Components/Board/Board.js
@@ -0,0 +1,7 @@
+import "./Board.css";
+
+const Board = () => {
+  return <div className="board"></div>;
+};
+
+export default Board;
--- frontend/src/Pages/Home/Home.js
+++ frontend/src/Pages/Home/Home.js
@@ -1,5 +1,11 @@
+import Board from "../../Components/Board/Board.js";
+
 const Home = () => {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <Board />
+    </div>
+  );
 };
 
 export default Home;
